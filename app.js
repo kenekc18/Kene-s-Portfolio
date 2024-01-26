8687e4455356ca62251f21ea6cf9afbfbcc1606a
kenekc18 <kenekc18@gmail.com>
2024-01-26 21:29:22 +0100

    reborn

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-let projects = document.querySelectorAll(".pro");
+let cards = document.querySelectorAll(".-card");
 let dark_hover = document.querySelector(".dark-hover");
 
-projects.forEach(project => {
-    project.addEventListener('mouseover', () => {
+cards.forEach(card => {
+    card.addEventListener('mouseover', () => {
         dark_hover.style.display = 'block';
     });
 
-    project.addEventListener('mouseout', () => {
+    card.addEventListener('mouseout', () => {
         dark_hover.style.display = 'none';
     });
 });
