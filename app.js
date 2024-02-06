e790c0d5fbb0d443b6837c56f748186bdc98a919
kenekc18 <kenekc18@gmail.com>
2024-02-06 14:14:21 +0100

    website

---
+++ app.js
@@ -0,0 +1,11 @@
+//NAVIGATION FUNCTION
+let smallNavItems = document.querySelector('.mobile-items');
+let NavBtn = document.querySelector('.hamburger-btn');
+
+NavBtn.addEventListener('click', ()=>{
+    smallNavItems.classList.add('d-mobile-items');
+})
+
+NavBtn.addEventListener('dblclick', ()=>{
+    smallNavItems.classList.remove('d-mobile-items');
+})
